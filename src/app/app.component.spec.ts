3e1597bde28b9b2b5c1520fc991363295572e999
nicoovillarr <nicolasvillar00@gmail.com>
2022-12-13 11:53:44 -0300

    Rename project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'juli'`, () => {
+  it(`should have as title '100ud'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('juli');
+    expect(app.title).toEqual('100ud');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('juli app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('100ud app is running!');
   });
 });
